dc7937e20029e4bde2a4440fbdc32d92699279ef
martinmainathuku-web <martinmainathuku@gmail.com>
2025-09-19 14:20:11 +0300

    Add files via upload

---
+++ programm displaying my units.c
@@ -0,0 +1,21 @@
+//introduction to programming
+/*
+Name:Martin Maina
+Reg no:PA106/28855/25
+Date:14/9/2025
+Description:program displaying my units
+*/
+#include<stdio.h>
+//main function
+int main()
+{
+printf("\n1 introduction to rogramming and problem solving");
+printf("\n2 computer organization");
+printf("\n analogue electronics");	
+printf("\n4 discrete mathematics");
+printf("\n5 mathematics for science");
+printf("\n6 physics");
+printf("\n7 communication and information literacy skils");
+	
+	return 0;
+}
---
+++ programm to display height,age,balance.c
@@ -0,0 +1,27 @@
+//programm to diplay height,age,ID number,bank balance
+/*
+Name:Martin Maina
+Reg no:PA106/28855/25
+Date:14/9/2025
+Description:Assignment displaying numerical values
+*/
+#include <stdio.h>
+int main()
+{
+	printf("My name!");
+	printf("\n1 Martin Maina");
+	
+	int height=6;
+	printf("\n2 height %d",height);
+	
+	int IDnumber=2768899;
+	printf("\n3 IDnumber %d",IDnumber);
+	
+	int Bankbalance=1000;
+	printf("\n4 Balancebalance %d",Bankbalance);
+	
+	int age=19;
+	printf("\n5 age %d",age);
+
+	return 0;
+}
