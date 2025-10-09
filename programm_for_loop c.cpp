45317399f385ed082e06e94d26c2fabfe89635a1
Martin Maina <martinmainathuku@gmail.com>
2025-10-09 11:47:47 +0300

    Add files via upload

---
+++ programm_for_loop c.cpp
@@ -0,0 +1,17 @@
+/*
+Name:Martin Maina Thuku
+Reg no:PA106/G/28855/25
+Descripton:program to show loop
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	for(i=100;i>=50;i--){
+		printf("%d\n",i);
+	}
+	
+	return 0;
+}
